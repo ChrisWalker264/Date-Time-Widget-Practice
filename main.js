9b694bbc83beee0a7105afb5e6b0e71ea5f4998c
ChrisWalker264 <101838158+ChrisWalker264@users.noreply.github.com>
2023-04-05 19:51:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const timeElement =document.querySelector(".time");
 const dateElement =document.querySelector(".date");
 
-/** *@param {Date} date */
+/** * @param {Date} date */
 function formatTime(date) {
     const hours12 = date.getHours() % 12 || 12;
     const minutes = date.getMinutes();
@@ -11,16 +11,17 @@ function formatTime(date) {
     return `${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
 };
 
-/** *@param {Date} date */
+/** * @param {Date} date */
 function formatDate(date) {
     const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    return `${DAYS[date.getDay()], ${MONTHS[date.getMonth()]}} ${date.getDate()} ${date.getFullYear}`;
+    return `DAYS[date.getDay()], ${MONTHS[date.getMonth()]}} ${date.getDate()} ${date.getFullYear}`;
+}
 
     setInterval(() => {
         const now = new Date();
 
         timeElement.textContext = formatTime(now);
         dateElement.textContext = formatDate(now);
-    } 200);
+    }, 200);
